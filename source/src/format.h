a9e52972490fe3c191c5567c14b7c735dc0ee151
ibelyaev <Ivan.Belyaev@cern.ch>
2016-11-14 17:35:51 +0100

    add a trivial replacement for boost::format

---
+++ source/src/format.h
@@ -0,0 +1,48 @@
+// ============================================================================
+#ifndef OSTAP_FORMAT_H 
+#define OSTAP_FORMAT_H 1
+// ============================================================================
+// Include files
+// ============================================================================
+// STD&STL
+// ============================================================================
+#include <string>
+// ============================================================================
+namespace Ostap
+{
+  // ============================================================================
+  /** very ugly way to avoid boost::format 
+   *  @author Vanya BELYAEV Ivan.Belyaev@itep.ru
+   */
+  // ============================================================================
+  /// format single number ..
+  std::string format ( const std::string& fmt    ,
+                       double             value  ) ;
+  /// format two numbers
+  std::string format ( const std::string& fmt    , 
+                       double             value1 ,
+                       double             value2 ) ;
+  /// format three numbers
+  std::string format ( const std::string& fmt    , 
+                       double             value1 , 
+                       double             value2 ,
+                       double             value3 ) ;
+  /// format four numbers
+  std::string format ( const std::string& fmt    ,
+                       double             value1 , 
+                       double             value2 ,
+                       double             value3 ,
+                       double             value4 ) ; 
+  /// format five numbers
+  std::string format ( const std::string& fmt    , 
+                       double             value1 , 
+                       double             value2 ,
+                       double             value3 ,
+                       double             value4 , 
+                       double             value5 ) ; 
+// ============================================================================
+}
+// ============================================================================
+//                                                                      The END 
+// ============================================================================
+#endif // OSTAP_FORMAT_H
