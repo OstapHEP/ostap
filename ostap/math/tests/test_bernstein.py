175994808c8d4390e7b1d468acf1c1a26f22468f
ibelyaev <Ivan.Belyaev@cern.ch>
2017-12-02 15:27:12 +0100

    ostap:  - extend test for bernstein polynomials

--- ostap/math/tests/test_bernstein.py
+++ ostap/math/tests/test_bernstein.py
@@ -35,7 +35,7 @@ def test_solve ():
     ## Bernstein polynomial with known roots
     bs = Ostap.Math.Bernstein (  0 , 1 , roots , croots )
 
-    ##  find root of Bernstein  polynomial 
+    ##  find roots of Bernstein  polynomial 
         
     rr = bs.solve()
     logger.info ('Roots found : %s' % list( rr ) )
@@ -158,7 +158,7 @@ def test_elevatereduce () :
 
 # ==============================================================================
 ## test  for polynomials 
-def test_poly1 () :
+def test_poly () :
     
     BP = Ostap.Math.Bernstein
     
@@ -172,7 +172,7 @@ def test_poly1 () :
         y = b(x)
         check_equality ( y , 1 , 'Invalid polynom value:' )
         
-    logger.info ('Constant poly is OK' )
+    logger.info ('Constant   poly is OK' )
 
     # evaluate the polynom
     for i in  b  : b[i] = random.uniform(-10,10)
@@ -185,7 +185,7 @@ def test_poly1 () :
         if not ymin <= y <= ymax : 
             raise ValueError ( 'Invalid polynom value y(%s)=%s (%s/%s)' % ( x , y , ymin , ymax ) )
  
-    logger.info ('Random poly is OK' )
+    logger.info ('Random     poly is OK' )
     
 # ==============================================================================
 ## test  for even polynomials 
@@ -209,7 +209,7 @@ def test_even () :
         check_equality ( y1 , y2  , 'Invalid BernsteinEven'  , 1.e-7 )
 
     
-    logger.info ('Even poly is OK' )
+    logger.info ('Even       poly is OK' )
 
 # ==============================================================================
 ## test  for monothonic polynomial 
@@ -252,18 +252,182 @@ def test_monothonic () :
         
     logger.info ('Decreasing poly is OK' )
 
+# =============================================================================
+## test for Convex positive polynmomials 
+def test_convex () :
+    """Test for Convex positive polynmomials 
+    """
+    
+    BPC = Ostap.Math.Convex
+    
+    b_11 = BPC ( 5 , 0 , 2 , True  , True  )
+    b_01 = BPC ( 5 , 0 , 2 , False , True  )
+    b_10 = BPC ( 5 , 0 , 2 , True  , False )
+    b_00 = BPC ( 5 , 0 , 2 , False , False )
+    
+    for b in ( b_11   , b_01 , b_10 , b_00 ) : 
+        for i in b : b[i] = random.uniform ( -10 , 10 ) 
+            
+    for i in range(500) :
+        
+        ##  note:  x1 <  x2 
+        x1 = random.uniform ( b.xmin() , b.xmax() )
+        x2 = random.uniform ( x1       , b.xmax() )
+        xm = 0.5 * ( x1 + x2 )
+        
+        for b in ( b_11   , b_01 , b_10 , b_00 ) :
+
+            b1 = b ( x1 ) 
+            b2 = b ( x2 ) 
+
+            if  b1 < 0 or b2 < 0 :
+                raise ValueError ( 'Invalid Convex value (b<0)' )
+
+            if b.increasing() and b1 > b2 :
+                    raise ValueError ( 'Invalid Convex Increasing' )
+                
+            if b.decreasing() and b1 < b2 :
+                raise ValueError ( 'Invalid Convex Decreasing' )
+            
+            bm = b ( xm ) 
+            if b.convex  () and ( b1 + b2 ) < 2 * bm :
+                    raise ValueError ( 'Invalid Convex!' )                    
+            if b.concave () and ( b1 + b2 ) > 2 * bm :
+                    raise ValueError ( 'Invalid Concave!' )
+                                
+    logger.info ('Convex     poly is OK' )
+
+
+# =============================================================================
+## test for Convex positive polynmomials 
+def test_convexonly () :
+    """Test for Convex-Only positive polynmomials 
+    """
+    
+    BPC = Ostap.Math.ConvexOnly
+    
+    b_11 = BPC ( 5 , 0 , 2 , True  )
+    b_01 = BPC ( 5 , 0 , 2 , False )
+    
+    for b in ( b_11   , b_01 ) : 
+        for i in b  : b[i] = random.uniform ( -10 , 10 ) 
+            
+    for i in range(500) :
+        
+        ##  note:  x1 <  x2 
+        x1 = random.uniform ( b.xmin() , b.xmax() )
+        x2 = random.uniform ( x1       , b.xmax() )
+        xm = 0.5 * ( x1 + x2 )
+        
+        for b in ( b_11   , b_01 ) :
+
+            b1 = b ( x1 ) 
+            b2 = b ( x2 ) 
+
+            if  b1 < 0 or b2 < 0 :
+                raise ValueError ( 'Invalid Convex value (b<0)' )
+
+            bm = b ( xm ) 
+            if b.convex  () and ( b1 + b2 ) < 2 * bm :
+                    raise ValueError ( 'Invalid Convex!' )                    
+            if b.concave () and ( b1 + b2 ) > 2 * bm :
+                    raise ValueError ( 'Invalid Concave!' )
+                                
+    logger.info ('ConvexOnly poly is OK' )
+
+
+# =============================================================================
+## test integration of 
+def test_integration () :
+    """Test for polynomial interation
+    """
+    
+    BP = Ostap.Math.Bernstein
+    
+    b = BP ( 5 , 0 , 2  )
+    for i in b  : b[i] = random.uniform ( -10 , 10 ) 
+
+    from ostap.math.integral import romberg 
+    
+    for i in range ( 500 ) :
+        
+        ##  note:  x1 <  x2 
+        x1 = random.uniform ( b.xmin() , b.xmax() )
+        x2 = random.uniform ( x1       , b.xmax() )
+
+        i1 = b.integral (      x1 , x2 )
+        i2 = romberg    ( b ,  x1 , x2 )
+
+        dd = ( i1 - i2 )
+        ds = ( abs(i1) + abs ( i2 ) )
+        if abs ( dd ) > ds * 1.e-7 :
+            raise ValueError ( 'Invalid Integrals!' )
+
+    logger.info ('Integration     is  OK' )
+
+# =============================================================================
+## test transformations
+def test_transformation () :
+    """Test for transformation
+    """
+    
+    BP = Ostap.Math.Bernstein
+    MS = Ostap.Math.Polynomial   ## monomial sum 
+    CS = Ostap.Math.ChebyshevSum ## chebyshev sum 
+    LS = Ostap.Math.LegendreSum  ## legendre sum 
+    
+    
+    b = BP ( 5 , 0 , 2  )
+
+    for i in range ( 50 ) :
+        
+        for i in b  : b[i] = random.uniform ( -10 , 10 ) 
+    
+        ## monomial sum 
+        ms = MS ( b  )
+        bm = BP ( ms ) 
+
+        ## chebyshev sum 
+        cs = CS ( b  )
+        bc = BP ( cs ) 
+
+        ## chebyshev sum 
+        ls = LS ( b  )
+        bl = BP ( ls ) 
+
+        for i in range( 100 ) :
+            
+            x1 = random.uniform ( b.xmin() , b.xmax() )
+
+            bv = b ( x1 )
+            
+            check_equality ( bv , ms(x1) , 'Invalid Bernstein->Monomial  transformation' )
+            check_equality ( bv , bm(x1) , 'Invalid Bernstein<-Monomial  transformation' )
+            
+            check_equality ( bv , cs(x1) , 'Invalid Bernstein->Chebyshev transformation' )
+            check_equality ( bv , bc(x1) , 'Invalid Bernstein<-Chebyshev transformation' )
+            
+            check_equality ( bv , ls(x1) , 'Invalid Bernstein->Legendre  transformation' )
+            check_equality ( bv , bl(x1) , 'Invalid Bernstein<-Legendre  transformation' )
+
+
+    logger.info ('Transformation  is  OK' )
 
 # =============================================================================
 if '__main__' == __name__ :
         
-    test_solve         ()
-    test_nroots        ()
-    test_interpolation ()
-    test_division      ()
-    test_elevatereduce ()
-    test_poly1         ()
-    test_even          ()
-    test_monothonic    ()
+    test_poly           ()
+    test_even           ()
+    test_monothonic     ()
+    test_convex         () 
+    test_convexonly     ()
+    test_solve          ()
+    test_nroots         ()
+    test_interpolation  ()
+    test_division       ()
+    test_elevatereduce  ()
+    test_integration    ()
+    test_transformation ()
     
 # =============================================================================
 # The END 
