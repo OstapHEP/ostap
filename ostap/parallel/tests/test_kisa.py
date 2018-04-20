8faa935f5112e04eef48e35437b2f47e19a11323
ibelyaev <Ivan.Belyaev@cern.ch>
2018-04-20 15:56:29 +0200

    update

--- ostap/parallel/tests/test_kisa.py
+++ ostap/parallel/tests/test_kisa.py
@@ -33,7 +33,8 @@ with timing('Prepare data') :
     from ostap.utils.utils import CleanUp
     tmpdir = CleanUp().tmpdir 
     import prepare_test_kisa as PTK
-    data   = PTK.prepare_data ( tmpdir , nfiles = 250 , nentries = 200000, silent = False ) 
+    ##data   = PTK.prepare_data ( tmpdir , nfiles = 250 , nentries = 200000, silent = False ) 
+    data   = PTK.prepare_data ( tmpdir , nfiles = 10 , nentries = 100 , silent = False ) 
     logger.info    ( 'DATA %s' % data  )
 
 # =============================================================================
@@ -104,11 +105,44 @@ def test_kisa2 () :
     ##logger.info ( 'Dataset: %s' % ds )
 
 
+## # =============================================================================
+## def test_kisa3 () :
+
+##     h1  = ROOT.TH1D('h1','',100,0,20)
+##     h1 += lambda x : x
+    
+##     from  ostap.trees.funcs import H1DFunc
+##     xh1 = H1DFunc ( histo = h1 , xvar = 'pt' ) 
+    
+##     from ostap.fitting.selectors import SelectorWithVars, Variable  
+##     variables = [
+##         ##Variable ( 'mass1' , 'mass(mu+mu-)' , 2 , 4 , lambda s : s.mass ) , 
+##         Variable   ( 'mass'  , 'mass(mu+mu-)' ,  3.09 , 3.11 ) , 
+##         Variable   ( 'c2dtf' , 'chi2(dtf)'    , -1    , 10   ) , 
+##         Variable   ( 'xpt'   , 'chi2(dtf)'    , -1    , 30   , xh1 ) , 
+##         ##Variable ( 'mass2' , 'mass(mu+mu-)' , 1 , 5 , MASS()  ) , 
+##         ##Variable ( 'mass3' , 'mass(mu+mu-)' , 1 , 5 , 'mass'  ) 
+##         ]
+
+    
+##     with timing('fill it!' ) :
+##         selector = SelectorWithVars  (
+##             variables = variables ,
+##             selection =  '2<=mass && mass<4 && 0<=c2dtf && c2dtf<5' ,
+##             silence   = False
+##             )
+##         chain =  data.chain
+##         st = chain.process ( selector , silent = False , shortcut = True )
+##         ds = selector.data
+##         del selector 
+
 # =============================================================================
 if '__main__' == __name__ :
 
     test_kisa  ()
     test_kisa2 ()
+    #test_kisa3 ()
+    
     pass
 
 
