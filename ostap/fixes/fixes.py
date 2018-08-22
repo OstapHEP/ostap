0bf6eb474697523ce8861ee3a70c261071d85f15
ibelyaev <Ivan.Belyaev@cern.ch>
2018-08-22 17:53:49 +0200

    small modification

--- ostap/fixes/fixes.py
+++ ostap/fixes/fixes.py
@@ -6,7 +6,7 @@
 #  @author Vanya BELYAEV Ivan.Belyaev@itep.ru
 #  @date   2016-02-23
 # =============================================================================
-"""Couple o fminot fixes for Ostap
+"""Couple of minor fixes for Ostap
 """
 # =============================================================================
 __version__ = '$Revision$'
@@ -14,7 +14,7 @@ __author__  = 'Vanya BELYAEV Ivan.Belyaev@itep.ru'
 __date__    = '2016-02-23'
 __all__     = () ## noting to import 
 # =============================================================================
-import ROOT 
+import ROOT, os  
 # =============================================================================
 from ostap.logger.logger import getLogger
 if '__main__' == __name__ : logger = getLogger ( 'ostap.fixes.fixes')
@@ -25,7 +25,7 @@ from ostap.logger.utils import mute
 with mute() :
     v = ROOT.RooRealVar()
     del v
-
+    
 try :
     enabled = ROOT.ROOT.IsImplicitMTEnabled ()
     if enabled : logger.debug ("ImplicitMT is  enabled")
@@ -35,7 +35,45 @@ except AttributeError :
     ROOT.ROOT.EnableImplicitMT     = lambda *_ : False
     ROOT.ROOT.DisableImplicitMT    = lambda *_ : False 
     logger.info ("``Enable/Disable''Implicit MT is not available") 
+
+# =============================================================================
+# Include path for ACLiC:
+# =============================================================================
+opath = ROOT.gSystem.GetIncludePath()
+logger.debug ( 'Old include ath: %s' % opath  )
+opath = opath.replace ( '-I' , ' ' ) . split ()
+## add gsl ? 
+opath.append ( '$OSTAPDIR/include' ) 
+
+npath  = []
+npath_ = []
+for item in opath :
+
+    if not item : continue
     
+    if   item[0] == '"' and item[-1]== '"' : item = item[1:-1]
+    elif item[0] == "'" and item[-1]== "'" : item = item[1:-1]
+    
+    nitem = os.path.expandvars (  item ) 
+    nitem = os.path.expandvars ( nitem ) 
+    nitem = os.path.expandvars ( nitem ) 
+    nitem = os.path.expanduser ( nitem ) 
+    nitem = os.path.expandvars ( nitem ) 
+
+    if os.path.exists ( nitem ) and os.path.isdir ( nitem ) :
+        if not item in npath and not nitem in npath_ :
+            if ' ' in item : npath.append ( '"' + item + '"')
+            else           : npath.append (       item      )
+            npath_.append ( nitem ) 
+            ## for CINT
+            ROOT.gROOT.ProcessLine ('.include  %s' % nitem )
+            
+if npath : npath[0] = '-I'+npath[0]
+npath = ' -I'.join(npath)
+ROOT.gSystem.SetIncludePath( npath ) 
+logger.debug ( 'New include path: %s' % npath  )
+# =============================================================================
+
     
 # =============================================================================
 if '__main__' == __name__ : 
