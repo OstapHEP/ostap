8c1869cc9fceb8d630b866ff633073088d2028ed
TatianaOvsiannikova <43534935+TatianaOvsiannikova@users.noreply.github.com>
2020-04-04 00:05:04 +0300

    Update test_tools_reweight2.py

--- ostap/tools/tests/test_tools_reweight2.py
+++ ostap/tools/tests/test_tools_reweight2.py
@@ -261,7 +261,7 @@ for iter in range ( 1 , maxIter + 1 ) :
         
         # =========================================================================
         ## 1a) create new "weighted" mcdataset
-       mcds=mcds_.clone()
+        mcds=mcds_.Сlone()
 
     with timing ( 'Add weight to MC-dataset' , logger = logger ) :
         ## 1b) add  "weight" variable to dataset 
