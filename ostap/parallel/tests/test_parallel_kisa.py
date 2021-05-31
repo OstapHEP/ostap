14c6855556a11da9c3703341151201c7ef8f42db
Vanya Belyaev <ivan.belyaev@cern.ch>
2021-05-31 14:14:25 +0200

    fix?

--- ostap/parallel/tests/test_parallel_kisa.py
+++ ostap/parallel/tests/test_parallel_kisa.py
@@ -205,7 +205,7 @@ def test_kisa3 () :
     
     from ostap.fitting.pyselectors import SelectorWithVars, Variable  
     variables = [
-        Variable ( 'mass1' , 'mass(mu+mu-)' , 2 , 4 , lambda s : s.mass ) , 
+        ## Variable ( 'mass1' , 'mass(mu+mu-)' , 2 , 4 , lambda s : s.mass ) , 
         Variable ( 'mass'  , 'mass(mu+mu-)' ,  3.09 , 3.11 ) , 
         Variable ( 'c2dtf' , 'chi2(dtf)'    , -1    , 10   ) , 
         ## Variable ( 'xpt'   , 'xpt'          , -1    , 30   , xh1 ) , 
