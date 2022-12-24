241c713aeb790f54314b3dec5faca1cc937b9b50
Vanya Belyaev <ivan.belyaev@cern.ch>
2022-12-24 14:00:56 +0100

    extend the test

--- ostap/math/tests/test_math_ve2.py
+++ ostap/math/tests/test_math_ve2.py
@@ -117,36 +117,144 @@ def test_math_ve2 ():
 # =============================================================================
 def test_math_ve3 ():
 
-    logger = getLogger("test_math_ve2")
+    logger = getLogger("test_math_ve3")
 
 
     funcs = ( bessel_J , bessel_Y ,bessel_I , bessel_K )
 
     rows = [ ( 'Function' , 'order' ,  'argument' , 'value' ) ]
     
-    for f in funcs :
+    from   ostap.utils.gsl       import gslCount
 
-        for n in range ( 0 , 4 ) : 
-            x   = random.uniform ( 0 , 1 )
-            row = f.__name__ , '%s' % n , '%s' % x , '%s' % f ( n , x )        
-            rows.append ( row )
-
-            x   = VE ( random.uniform ( 1 , 2 ) , 0.1 **2 ) 
-            row = f.__name__ , '%s' % n , '%s' % x , '%s' % f ( n , x )        
-            rows.append ( row )
-
-        n   = random.uniform ( -1 , 3 )
-        row = f.__name__ , '%+.3f' % n , '%s' % x , '%s' % f ( n , x )        
-        rows.append ( row )
+    with gslCount() :
         
-        x   = VE ( random.uniform ( 1 , 2 ) , 0.1 **2 ) 
-        row = f.__name__ , '%+.3f' % n , '%s' % x , '%s' % f ( n , x )        
-        rows.append ( row )
+        for f in funcs :
+            
+            for n in range ( 0 , 4 ) : 
+                x   = random.uniform ( 0 , 1 )
+                row = f.__name__ , '%s' % n , '%s' % x , '%s' % f ( n , x )        
+                rows.append ( row )
+                
+                x   = VE ( random.uniform ( 1 , 2 ) , 0.1 **2 ) 
+                row = f.__name__ , '%s' % n , '%s' % x , '%s' % f ( n , x )        
+                rows.append ( row )
+                
+            n   = random.uniform ( 1 , 3 )
+            row = f.__name__ , '%+.3f' % n , '%s' % x , '%s' % f ( n , x )        
+            rows.append ( row )
+            
+            x   = VE ( random.uniform ( 1 , 2 ) , 0.1 **2 ) 
+            row = f.__name__ , '%+.3f' % n , '%s' % x , '%s' % f ( n , x )        
+            rows.append ( row )
 
     title = 'Functions'
     table = T.table ( rows, title = title , prefix = '# ' , alignment = 'lccc' ) 
     logger.info ( '%s\n%s' % ( title , table ) ) 
 
+
+# =============================================================================
+def test_math_ve4 ():
+
+    logger = getLogger("test_math_ve4")
+
+    import ostap.math.derivative as     D
+    import ostap.math.integral   as     I
+    from   ostap.core.core       import Ostap, SE  
+    from   ostap.math.models     import f1_draw
+    from   ostap.plotting.canvas import use_canvas
+    from   ostap.utils.utils     import wait 
+    from   ostap.utils.gsl       import gslCount
+    
+    functions = [
+        ##
+        ( bessel_J , 0   , Ostap.Math.der_bessel_Jn  ) ,
+        ( bessel_J , 0.0 , Ostap.Math.der_bessel_Jnu ) ,
+        ( bessel_J , 2   , Ostap.Math.der_bessel_Jn  ) ,
+        ( bessel_J , 2.0 , Ostap.Math.der_bessel_Jnu ) ,
+        ( bessel_J , 1.5 , Ostap.Math.der_bessel_Jnu ) ,
+        ( bessel_J , 2.5 , Ostap.Math.der_bessel_Jnu ) ,
+        ##
+        ( bessel_Y , 0   , Ostap.Math.der_bessel_Yn  ) ,
+        ( bessel_Y , 0.0 , Ostap.Math.der_bessel_Ynu ) ,
+        ( bessel_Y , 2   , Ostap.Math.der_bessel_Yn  ) ,
+        ( bessel_Y , 2.0 , Ostap.Math.der_bessel_Ynu ) ,
+        ( bessel_Y , 1.5 , Ostap.Math.der_bessel_Ynu ) ,
+        ( bessel_Y , 2.5 , Ostap.Math.der_bessel_Ynu ) ,
+        ##
+        ( bessel_I , 0   , Ostap.Math.der_bessel_In  ) ,
+        ( bessel_I , 0.0 , Ostap.Math.der_bessel_Inu ) ,
+        ( bessel_I , 2   , Ostap.Math.der_bessel_In  ) ,
+        ( bessel_I , 2.0 , Ostap.Math.der_bessel_Inu ) ,
+        ( bessel_I , 1.5 , Ostap.Math.der_bessel_Inu ) ,
+        ( bessel_I , 2.5 , Ostap.Math.der_bessel_Inu ) ,
+        ##
+        ##
+        ( bessel_K , 0   , Ostap.Math.der_bessel_Kn  ) ,
+        ( bessel_K , 0.0 , Ostap.Math.der_bessel_Knu ) ,
+        ( bessel_K , 2   , Ostap.Math.der_bessel_Kn  ) ,
+        ( bessel_K , 2.0 , Ostap.Math.der_bessel_Knu ) ,
+        ( bessel_K , 1.5 , Ostap.Math.der_bessel_Knu ) ,
+        ( bessel_K , 2.5 , Ostap.Math.der_bessel_Knu ) ,
+        ##
+        ]
+
+    keep = []
+    
+    scale1 = 1.e+15 
+    scale2 = 1.e+12 
+    rows = [ ( 'Derivative' , 'nu' ,
+               'delta [%.0g]' % ( 1/scale1 )  ,
+               'amean [%.0g]' % ( 1/scale1 )  , 'amax [%.0g]' % ( 1/scale1 )  , 
+               'rmean [%.0g]' % ( 1/scale2 )  , 'rmax [%.0g]' % ( 1/scale2 )  ) ]
+    
+    xmin, xmax = 1 , 7
+    
+    with gslCount () :
+        
+        for fun , nu, der in functions :
+            
+            ff = lambda x : fun ( nu , x )
+            fd = lambda x : der ( nu , x )
+            dn = D.Derivative  ( ff )
+            
+            title = '%s_%s' % ( fun.__name__ , nu )
+            
+            with use_canvas ( title ) , wait ( 1 ) :
+                f1_draw ( fd , xmin = xmin , xmax = xmax , linecolor = 2 )
+                f1_draw ( dn , xmin = xmin , xmax = xmax , linecolor = 4 )
+                
+                dd = lambda x : ( fd ( x ) - dn ( x ) ) ** 2
+                d2 = I.integral ( dd , xmin = xmin , xmax = xmax ) / ( xmax - xmin ) 
+                d  = math.sqrt ( d2 ) / scale1 
+                
+                cnt1 = SE()  
+                cnt2 = SE()  
+                for i in range  ( 10000 ) :
+                    xx   = random.uniform ( xmin , xmax )
+                    
+                    v1  = fd ( xx )
+                    v2  = dn ( xx ) 
+                    cnt1 += abs ( v1 - v2 )                                * scale1 
+                    cnt2 += abs ( v1 - v2 ) / ( abs  ( v1 ) + abs ( v2 ) ) * scale2 
+                    
+                dmax1  = cnt1.max()          
+                dmean1 = cnt1.mean().value()  
+                dmax2  = cnt2.max()          
+                dmean2 = cnt2.mean().value()  
+                row  = fun.__name__ , '%4s' % nu , '%.4g' % d  , \
+                       '%.4g' % dmax1 , '%.4g' % dmean1 , \
+                       '%.4g' % dmax2 , '%.4g' % dmean2 
+                rows.append ( row )
+                
+            
+            keep.append (   ( ff, fd , dn , dd ) ) 
+        
+            
+    title = 'Derivatives'
+    table = T.table ( rows, title = title , prefix = '# ' , alignment = 'lcccc' ) 
+    logger.info ( '%s\n%s' % ( title , table ) ) 
+        
+        
     
 # =============================================================================
 if '__main__' == __name__ :
@@ -154,6 +262,7 @@ if '__main__' == __name__ :
     test_math_ve1 ()
     test_math_ve2 ()
     test_math_ve3 ()
+    test_math_ve4 ()
     
 # =============================================================================
 ##                                                                      The END 
