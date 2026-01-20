06dee95d39fadda4229c4647a3493d57679f85dc
Vanya BELYAEV <Ivan.Belyaev@cern.ch>
2026-01-20 18:37:18 +0100

    rename parallel_gof1d to parallel_gof

---
+++ ostap/parallel/parallel_gof.py
@@ -0,0 +1,158 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# ==========================================================================================
+## @file ostap/parallel/parallel_gof1d.py
+#  Make paralllel toys for Goodness-of-fit estimates 
+#  @see ostap.stats.gof1d 
+#  @see ostap.stats.gof_simfit 
+#  @date   2024-11-21
+#  @author Vanya  BELYAEV Ivan.Belyaev@cern.ch
+# =============================================================================
+""" Make paralllel toys for Goodness-of-fit estimates 
+- see ostap.stats.gof1d
+- see ostap.stats.gof_simfit 
+"""
+# =============================================================================
+__author__  = 'Vanya BELYAEV  Ivan.Belyaev@itep.ru'
+__date__    = "2020-01-18"
+__version__ = '$Revision$'
+__all__     = (
+    'parallel_goftoys' , 
+    )
+# =============================================================================
+from   ostap.parallel.parallel import Task, WorkManager
+from   ostap.core.ostap_types  import string_types, integer_types
+from   ostap.utils.basic       import numcpu , typename 
+import ROOT
+# =============================================================================
+# logging 
+# =============================================================================
+from ostap.logger.logger import getLogger 
+if '__main__' ==  __name__ : logger = getLogger ( 'ostap.paralell.parallel_gof' )
+else                       : logger = getLogger ( __name__                      )
+# =============================================================================
+## @class GoFTask
+#  Simple task object 
+#  @author Vanya BELYAEV Ivan.Belyaev@itep.ru
+#  @date   2020-01-18 
+class GoFTask (Task) :
+    """ The GoF task object
+    """
+    ## 
+    def __init__ ( self , gof  ) : 
+        
+        self.__gof        = gof 
+        self.__the_output = None 
+        
+    @property
+    def the_output ( self ) :
+        return self.__the_output
+    @the_output.setter 
+    def the_output ( self , value ) :
+        self.__the_output = value 
+    
+    ## initialize the local task, no action 
+    def initialize_local   ( self ) : self.__the_output = None 
+
+    ## initialize the remote task, treat the random numbers  
+    def initialize_remote  ( self , jobid = -1 ) :
+        """ Initialize the remote task, treat the random numbers  
+        """
+        import random, ROOT
+        from ostap.parallel.utils import random_random
+        random_random ( jobid )        
+        return self.initialize_local() 
+        
+    ## get the results 
+    def results ( self ) :
+        return self.__the_output
+    
+    ## merge results of toys 
+    def merge_results ( self , result , jobid = -1 ) :
+        """ Merge results of toys
+        """
+        if not self.__the_output : self.__the_output  = result
+        else                     : self.__the_output += result 
+
+    # =========================================================================
+    ## the actual processing of toys 
+    def process ( self , jobid , nToys ) :
+        """ The actual processing of toys 
+        """
+        ##
+        from   ostap.stats.gof1d       import GoF1D     , GoF1DToys
+        from   ostap.stats.gof_simfit  import GoFSimFit , GoFSimFit1D , GoFSimFit1DToys
+        ## 
+        if   isinstance ( self.__gof , GoF1D       ) : toys = GoF1DToys       ( gof = self.__gof )
+        elif isinstance ( self.__gof , GoFSimFit1D ) : toys = GoFSimFit1DToys ( gof = self.__gof )
+        elif isinstance ( self.__gof , GoFSimFit   ) : toys = gof
+        else :
+            raise TypeError ( "Invalid `gof` type : %s" % typename ( self.__gof ) )
+        
+        toys.run ( nToys    = nToys ,
+                   parallel = False ,
+                   silent   = True  )
+        
+        ## del self.__gof        
+        #
+        return toys 
+
+# =============================================================================
+## Run GoF1D/GoFSimFit toys in parallel 
+def parallel_goftoys ( gof             ,
+                       nToys    = 1000 ,
+                       nSplit   = 0    ,
+                       silent   = True , 
+                       progress = True , **kwargs ) :
+    """ Run GoF1D/GoFSimFit toys in parallel 
+    """
+    from   ostap.stats.gof1d       import GoF1D     , GoF1DToys
+    from   ostap.stats.gof_simfit  import GoFSimFit , GoFSimFit1D , GoFSimFit1DToys
+    
+    assert isinstance ( gof ,  ( GoF1D, GoFSimFit , GoFSimFit1D ) ) , \
+        'Invalid "gof" type : %s' % typename ( gof )
+    
+    assert isinstance ( nToys  , integer_types ) and 0 < nToys  ,\
+        'Invalid "nToys"  argument %s/%s' % ( nToys  , type ( nToys  ) )
+    
+    if not nSplit : nSplit = max ( 2 , 2 * numcpu() ) 
+    
+    assert isinstance ( nSplit , integer_types ) and 0 < nSplit ,\
+        'Invalid "nSplit" argument %s/%s' % ( nSplit , type ( nSplit ) )
+
+    if nSplit < 2 or numcpu ()  < 2 :
+
+        if   isinstance ( gof , GoF1D       ) : toys = GoF1DToys     ( gof )
+        elif isinstance ( gof , GoFSimFit1D ) : toys = GoFSimFitToys ( gof )
+        elif isinstance ( gof , GoFSimFit   ) : toys = gof 
+        
+        toys.run ( nToys    = nToys                  ,
+                   parallel = False                  ,
+                   silent   = silent or not progress )
+        
+        return toys
+
+    ## create work manager 
+    wmgr  = WorkManager ( silent   = silent and not progress ,
+                          progress = progress or not silent  , **kwargs )
+
+    ## create the task 
+    task  = GoFTask ( gof = gof )
+
+    ## create/split arguments
+    
+    if nToys <= nSplit : params = nToys * [ 1 ]
+    else : 
+        size , rem = divmod ( nToys , nSplit )
+        if rem : params = [ size + rem ] + ( nSplit - 1 ) * [ size ]
+        else   : params =                    nSplit       * [ size ]
+
+    ## start parallel processing! 
+    wmgr.process ( task , params  )
+    
+    return task.results () 
+
+    
+# =============================================================================
+##                                                                      The END 
+# =============================================================================
